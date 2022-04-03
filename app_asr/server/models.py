e4a62065a9031d874fadd235af083aec57dabca8
Eduardo Carmona <samuelcarmona456@gmail.com>
2022-04-03 16:05:06 -0500

    Session

--- app_asr/server/models.py
+++ app_asr/server/models.py
@@ -1,3 +1,14 @@
 from datetime import datetime
 from time import time
-from sqlalchemy import Column, ForeignKey, 
+from flask_sqlalchemy import SQLAlchemy
+
+from sqlalchemy import ForeignKey, Column, Integer
+from sqlalchemy.orm import relationship
+
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'Users'
+    username = db.Column(db.String(20), primary_key=True)
+    password = db.COlumn(db.String(20)) 
